7bc160a4d1b684c714ecbaaa43ea33f45d0daf2f
Daniel Oh <daniel.oh@heliolytics.com>
2020-04-07 19:43:50 -0400

    change file names

--- application.py
+++ application.py
@@ -4,9 +4,9 @@ from app import create_app
 
 
 # config_name = os.getenv("FLASK_CONFIG")
-app = create_app("production")
-CORS(app)
+application = create_app("production")
+CORS(application)
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(application, host="0.0.0.0", port=8080)

8a4611f9b8b2e73d9763ffb49fd548359066ae4c
Daniel Oh <daniel.oh@heliolytics.com>
2020-04-07 18:50:08 -0400

    push to prod

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from app import create_app
 
 
 # config_name = os.getenv("FLASK_CONFIG")
-app = create_app("development")
+app = create_app("production")
 CORS(app)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")

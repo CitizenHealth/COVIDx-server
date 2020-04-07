c56baa0f6b9c44f9e0ff4ef26f4914767e552fcc
Daniel Oh <daniel.oh@heliolytics.com>
2020-04-07 19:54:59 -0400

    adding the new endpoint

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,3 @@
 # SQLALCHEMY_DATABASE_URI = 'mysql://admin@localhost/covidx_db'
-SQLALCHEMY_DATABASE_URI = 'mysql://admin:^zT_UIsJ5Au1(v6e@aajqxze5zt4e94.cxnedy40wg8c.us-west-2.rds.amazonaws.com/covidx_db'
+SQLALCHEMY_DATABASE_URI = 'mysql://admin:^zT_UIsJ5Au1(v6e@aa19bixxzoxyunw.cxnedy40wg8c.us-west-2.rds.amazonaws.com/covidx_db'
 SECRET_KEY = 'bdc7a4581f8195e3e3bfbe1df5dc5a0de401b5a912e8e86a'

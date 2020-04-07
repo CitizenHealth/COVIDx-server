2f3083eeb104e700d5bb52d11870d60d88aa46f5
Daniel Oh <daniel.oh@heliolytics.com>
2020-04-06 23:46:09 -0400

    can't figure out how flask run works with eb so this'll do

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from flask_cors import CORS
 from app import create_app
 
 
-config_name = os.getenv("FLASK_CONFIG")
-app = create_app(config_name)
+# config_name = os.getenv("FLASK_CONFIG")
+app = create_app("development")
 CORS(app)
 
 if __name__ == "__main__":

39f95d810fffcff0e143fae193a58fbeddae6fd1
Daniel Oh <daniel.oh@heliolytics.com>
2020-04-14 22:17:53 -0400

    syntax error; state and county results should work now

--- app/location/views.py
+++ app/location/views.py
@@ -7,9 +7,9 @@ def all_location():
     return LocationActions.get_all_users()
 
 @location.route('/get_county_results', methods=['GET'])
-def get_all_states():
+def get_results_county():
     return LocationActions.get_county_results()
 
 @location.route('/get_state_results', methods=['GET'])
-def get_all_states():
+def get_results_state():
     return LocationActions.get_state_results()

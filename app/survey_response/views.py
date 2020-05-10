b90eb32051902505844c960124f45a3fb12017e8
Daniel Oh <daniel.oh@heliolytics.com>
2020-05-10 19:17:16 -0400

    fixing issues with survey response table

--- app/survey_response/views.py
+++ app/survey_response/views.py
@@ -1,24 +1,31 @@
 from flask import request, jsonify
 from app import db
 from datetime import datetime
+import requests
+import firebase_admin
+from models.survey_response import SurveyResponse
 
 from models.user import User
 
 from . import survey_response
 
-
+# getting error, need to check data going through
 @survey_response.route("/create_survey_response", methods=["POST"])
 def create_survey_response():
     try:
-        request_body = request.get_json()
-        fb_id = request_body.get('firebase_id')
-        user_data = User.query.filter_by(firebase_id=fb_id).first()
+        authorization = request.headers.get("Authorization")
+        token = authorization.replace('Bearer: ', '', 1)
+        fb_id = firebase_admin.auth.verify_id_token(token)['uid']
+
+        user_data = User.query.filter_by(firebase_id=fb_id).first().as_json
         user_id = user_data['user_id']
         # create new dictionary with parsed data out of the request body
-        req_data = {k:(datetime.strptime(v, "%Y-%m-%d") if k.endswith("_date") 
+        request_body = request.get_json()
+        req_data = {k:(datetime.strptime(v, "%Y-%m-%d") if k.endswith("_date") and v is not None
                     else float(v) if k=="therm_temp" else v if k!="firebase_id" else None) 
                     for k, v in request_body.items()}
-        req_data["datetime_submitted"] = datetime.now()
+        req_data["submitted_date"] = datetime.now()
+        req_data["user_id"] = user_id
         # throw this into the survey response model
         survey_response = SurveyResponse(**req_data)
         db.session.add(survey_response)
@@ -27,6 +34,7 @@ def create_survey_response():
 
     except Exception as e:
         db.session.rollback()
+        print(e)
         return jsonify(message=f"error {str(e)}"), 404
 
     finally:
